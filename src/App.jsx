2ad0abae6d977227c508b703f4b721ea8ad49a9b
Sarthak Tripathi <developersarthakg@gmail.com>
2026-05-28 23:32:41 +0530

    Initial commit counter app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Counter from './Counter'
+
+function App() {
+  return (
+    <div><p>This is the react application that would lead me to 50 lakhs of package.</p>
+    <Counter />
+    </div>
+  )
+}
+
+export default App
