c361c48f17155eed81ac065d99e9f27fa73ced02
Sarthak Tripathi <developersarthakg@gmail.com>
2026-06-02 20:22:32 +0530

    Feature - updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <div><p>This is the react application that would lead me to 50 lakhs of package.</p>
     <Counter />
     <Apple />
+    <Banana />
     </div>
   )
 }
@@ -14,6 +15,15 @@ function Apple(){
   return(
     <div>
       <h2>5 lakhs mobile sold in a minute</h2>
+    </div>
+  )
+}
+
+
+function Banana(){
+  return(
+    <div>
+      <h2>50 lakhs Banana sold in a minute</h2>
     </div>
   )
 }
