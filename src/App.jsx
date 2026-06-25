7e903fcec76829d54fb91e10cfe8fa4a261be4a4
Sarthak Tripathi <developersarthakg@gmail.com>
2026-06-25 05:57:29 +0530

    prettier format

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import Counter from './Counter'
-import RevisonCounter from './RevisonCounter'
+import React from "react";
+import Counter from "./Counter";
+import RevisonCounter from "./RevisonCounter";
 
 function App() {
   return (
-    <div><p>This is the react application that would lead me to 50 lakhs of package.</p>
-    <Counter />
-    <Apple />
-    <Cars />
-    <RevisonCounter />
+    <div>
+      <p>
+        This is the react application that would lead me to 50 lakhs of package.
+      </p>
+      <Counter />
+      <Apple />
+
+      <Cars />
+      <RevisonCounter />
     </div>
-  )
+  );
 }
 
-function Apple(){
-  return(
+function Apple() {
+  return (
     <div>
       <h2>5 lakhs mobile sold in a minute</h2>
     </div>
-  )
+  );
 }
 
-function Cars(){
-  return(
+function Cars() {
+  return (
     <div>
       <h2>10 lakhs cars sold in a minute</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/RevisonCounter.jsx
+++ src/RevisonCounter.jsx
@@ -1,20 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-function RevisonCounter () {
+function RevisonCounter() {
+  const [count, setCount] = useState(0);
 
-    const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Counter Revision App</h1>
 
-    return(
-        <div>
-        <h1>Counter Revision App</h1>
+      <button onClick={() => setCount(count + 1)}>
+        <h3>Increment Counter</h3>{" "}
+      </button>
 
-        
-        <button onClick = {() =>setCount(count+1)} ><h3>Increment Counter</h3> </button>
-       
-        <button onClick ={() => setCount(count-1)} >  <h3>Decrement Counter</h3> </button>
-       <h2>Counter Value {count}</h2>
-        </div>
-    );
-};
+      <button onClick={() => setCount(count - 1)}>
+        {" "}
+        <h3>Decrement Counter</h3>{" "}
+      </button>
+      <h2>Counter Value {count}</h2>
+    </div>
+  );
+}
 
 export default RevisonCounter;
